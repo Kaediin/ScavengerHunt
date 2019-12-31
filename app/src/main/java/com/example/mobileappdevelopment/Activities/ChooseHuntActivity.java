a935dbeacc2a609512e1fadbe6669c52ad67aa55
Kaediin <skaedin@gmail.com>
2019-12-31 13:28:26 +0100

    Implement search bar #6

--- app/src/main/java/com/example/mobileappdevelopment/Activities/ChooseHuntActivity.java
+++ app/src/main/java/com/example/mobileappdevelopment/Activities/ChooseHuntActivity.java
@@ -1,6 +1,7 @@
 package com.example.mobileappdevelopment.Activities;
 
 import android.os.Bundle;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +10,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mobileappdevelopment.Adapters.RecycleViewAdapter;
-import com.example.mobileappdevelopment.DataUtils.DataHunt;
 import com.example.mobileappdevelopment.Model.Hunt;
 import com.example.mobileappdevelopment.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -27,7 +27,9 @@ public class ChooseHuntActivity extends AppCompatActivity {
 
     private List<String> titles = new ArrayList<>();
     private List<String> authors = new ArrayList<>();
-    private List<Hunt> hunts;
+    private List<Hunt> hunts = new ArrayList<>();
+
+    private SearchView searchView;
 
     private FirebaseFirestore fb;
 
@@ -36,10 +38,7 @@ public class ChooseHuntActivity extends AppCompatActivity {
         super.onCreate(savedInstances);
         setContentView(R.layout.activity_choose_hunt);
 
-        // data to populate the RecyclerView with
-        titles = DataHunt.getTitles();
-        authors = DataHunt.getAuthors();
-        hunts = new ArrayList<>();
+        searchView = findViewById(R.id.search_view);
 
         // connect to database
         fb = FirebaseFirestore.getInstance();
@@ -50,20 +49,38 @@ public class ChooseHuntActivity extends AppCompatActivity {
         // display the list in a recycle layout
         display();
 
+        setupQuery();
+
     }
 
-    public void populateList(){
+    public void setupQuery(){
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                showSearched(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                showSearched(s);
+                return false;
+            }
+        });
+    }
+
+    public void populateList() {
 
         Task<QuerySnapshot> query = fb.collection("Scavenger_Hunts").get();
         query.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                for (QueryDocumentSnapshot query : Objects.requireNonNull(task.getResult())){
+                for (QueryDocumentSnapshot query : Objects.requireNonNull(task.getResult())) {
                     String huntStringFromDb = query.getString("HuntFile");
                     Gson gson = new Gson();
                     Hunt hunt = gson.fromJson(huntStringFromDb, Hunt.class);
-//                    titles.add(hunt.getTitle());
-//                    authors.add(hunt.getAuthor());
+                    titles.add(hunt.getTitle());
+                    authors.add(hunt.getAuthor());
                     hunts.add(hunt);
                 }
                 display();
@@ -71,7 +88,7 @@ public class ChooseHuntActivity extends AppCompatActivity {
         });
     }
 
-    public void display(){
+    public void display() {
         // set up the RecyclerView
         RecyclerView recyclerView = findViewById(R.id.rvHunts);
         RecycleViewAdapter adapter = new RecycleViewAdapter(this, titles, authors);
@@ -81,5 +98,27 @@ public class ChooseHuntActivity extends AppCompatActivity {
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
                 1);
         recyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
+    public void showSearched(String s){
+        List<Hunt> dynamic_list = new ArrayList<>();
+
+        for (Hunt hunt : hunts){
+            if (hunt.getTitle().toUpperCase().contains(s.toUpperCase()) || hunt.getAuthor().toUpperCase().contains(s.toUpperCase())){
+                dynamic_list.add(hunt);
+            }
+        }
+
+        titles.clear();
+        authors.clear();
+        for (Hunt hunt : dynamic_list){
+            titles.add(hunt.getTitle());
+            authors.add(hunt.getAuthor());
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.rvHunts);
+        RecycleViewAdapter adapter = new RecycleViewAdapter(this, titles, authors);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
