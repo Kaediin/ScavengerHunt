c197a8dd4e9bafbb7a5bb55535f44901a2a58322
Kaediin <skaedin@gmail.com>
2019-12-31 12:30:39 +0100

    Change permission redirects #11

--- app/src/main/java/com/example/mobileappdevelopment/Activities/MainActivity.java
+++ app/src/main/java/com/example/mobileappdevelopment/Activities/MainActivity.java
@@ -141,8 +141,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == 800) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
         }
